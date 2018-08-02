04c89dcdba5688d2b7cdbe008addbf126defd487
Michael Hartley <mtjhartley@gmail.com>
2018-08-01 19:27:53 -0700

    added emptyactivity as searchactivity

---
+++ app/src/main/java/com/mtjhartley/wikipedia/activities/SearchActivity.kt
@@ -0,0 +1,13 @@
+package com.mtjhartley.wikipedia.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mtjhartley.wikipedia.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
