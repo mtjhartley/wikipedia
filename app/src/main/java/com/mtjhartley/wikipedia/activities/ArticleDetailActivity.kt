73283bc7294ec6d43a813e2bc76e73ad1cbe3c91
Michael Hartley <mtjhartley@gmail.com>
2018-08-01 19:46:07 -0700

    updated with views and layout

--- app/src/main/java/com/mtjhartley/wikipedia/activities/ArticleDetailActivity.kt
+++ app/src/main/java/com/mtjhartley/wikipedia/activities/ArticleDetailActivity.kt
@@ -2,12 +2,24 @@ package com.mtjhartley.wikipedia.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.mtjhartley.wikipedia.R
+import kotlinx.android.synthetic.main.activity_article_detail.*
 
 class ArticleDetailActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_detail)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item!!.itemId == android.R.id.home){
+            finish()
+        }
+        return true;
     }
 }
--- app/src/main/java/com/mtjhartley/wikipedia/activities/SearchActivity.kt
+++ app/src/main/java/com/mtjhartley/wikipedia/activities/SearchActivity.kt
@@ -1,13 +1,61 @@
 package com.mtjhartley.wikipedia.activities
 
+import android.app.SearchManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
 import com.mtjhartley.wikipedia.R
+import kotlinx.android.synthetic.main.activity_article_detail.*
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item!!.itemId == android.R.id.home){
+            finish()
+        }
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+        val searchItem = menu!!.findItem(R.id.action_search)
+
+        val searchmanager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = searchItem!!.actionView as SearchView
+        searchView.setSearchableInfo(searchmanager.getSearchableInfo(componentName))
+        searchView.setIconifiedByDefault(false)
+        searchView.requestFocus()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+                println("updated search")
+                return false
+            }
+        })
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
+
